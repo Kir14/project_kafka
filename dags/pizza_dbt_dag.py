db609686fa4baeda94dee8a35480d6036ed871d4
Kirill <kirill.kaleychik@mail.ru>
2026-03-23 09:51:08 +0300

    Airflow dbt run

--- dags/pizza_dbt_dag.py
+++ dags/pizza_dbt_dag.py
@@ -15,16 +15,37 @@ with DAG(
 ) as dag:
 
     # Run dbt run
-    run_dbt = BashOperator(
+    run_seed = BashOperator(
+        task_id='dbt_run_seed',
+        # We point to the project folder we mounted in docker-compose
+        bash_command='''
+            cd /opt/airflow/project && \
+            export DBT_LOG_PATH=/tmp/dbt_logs && \
+            export DBT_TARGET_PATH=/tmp/dbt_target && \
+            dbt seed --profiles-dir .dbt
+        '''
+    )
+
+    run_revenue = BashOperator(
         task_id='dbt_run_revenue',
         # We point to the project folder we mounted in docker-compose
-        bash_command='cd /opt/airflow/project && dbt run --profiles-dir .dbt'
+        bash_command='''
+            cd /opt/airflow/project && \
+            export DBT_LOG_PATH=/tmp/dbt_logs && \
+            export DBT_TARGET_PATH=/tmp/dbt_target && \
+            dbt run -s revenue_by_pizza --profiles-dir .dbt
+        '''
     )
 
     # Run dbt test
     test_dbt = BashOperator(
         task_id='dbt_test_quality',
-        bash_command='cd /opt/airflow/project && dbt test --profiles-dir .dbt'
+        bash_command='''
+            cd /opt/airflow/project && \
+            export DBT_LOG_PATH=/tmp/dbt_logs && \
+            export DBT_TARGET_PATH=/tmp/dbt_target && \
+            dbt test --profiles-dir .dbt
+        '''
     )
 
-    run_dbt >> test_dbt
+    run_seed >> run_revenue >> test_dbt
